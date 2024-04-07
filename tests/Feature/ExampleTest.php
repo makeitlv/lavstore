187deb870d3d0df1b1a6fc613d8d69bcb2c4a777
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2024-04-07 19:11:56 +0300

    Installed pest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,10 @@
 
 declare(strict_types=1);
 
-namespace Makeitlv\Lavstore\Tests\Feature;
+use function Pest\Laravel\{get};
 
-use Makeitlv\Lavstore\Tests\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function testTheApplicationReturnsASuccessfulResponse(): void
-    {
-        $response = $this->get('/lavstore/');
-
-        $response->assertStatus(200);
-        $response->assertContent('Hello World');
-    }
-}
+it('has a hello page', function () {
+    get('/lavstore/')
+        ->assertStatus(200)
+        ->assertContent('Hello World');
+});
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,17 +2,6 @@
 
 declare(strict_types=1);
 
-namespace Makeitlv\Lavstore\Tests\Unit;
-
-use Makeitlv\Lavstore\Tests\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function testThatTrueIsTrue(): void
-    {
-        $this->assertTrue(true);
-    }
-}
+test('example', function () {
+    expect(true)->toBeTrue();
+});
