912b4fd3b67cabbe86b21bafacbc9e38eeebf314
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2024-05-06 17:55:41 +0300

    Added onion rules for architecture

--- tests/Architecture/OnionArchitectureTest.php
+++ tests/Architecture/OnionArchitectureTest.php
@@ -10,15 +10,46 @@ use PHPat\Test\PHPat;
 
 final class OnionArchitectureTest
 {
-    public function testDomainDoesNotDependOnOtherLayers(): Rule
+    public function testModuleDomainDoesNotDependOnOtherLayers(): Rule
     {
         return PHPat::rule()
-            ->classes(Selector::inNamespace('Makeitlv\Lavstore\Domain'))
+            ->classes(Selector::inNamespace('Makeitlv\Lavstore\Module\.*\Domain'))
             ->shouldNotDependOn()
             ->classes(
-                Selector::inNamespace('Makeitlv\Lavstore\Application'),
-                Selector::inNamespace('Makeitlv\Lavstore\Infrastructure'),
+                Selector::inNamespace('Makeitlv\Lavstore\Module\.*\Application'),
+                Selector::inNamespace('Makeitlv\Lavstore\Module\.*\Infrastructure'),
             )
-            ->because('This will break our architecture, implement it another way!');
+            ->because('Domain layer in all modules should not depend on other layers');
+    }
+
+    public function testModuleApplicationDependsOnlyOnDomain(): Rule
+    {
+        return PHPat::rule()
+            ->classes(Selector::inNamespace('Makeitlv\Lavstore\Module\.*\Application'))
+            ->shouldNotDependOn()
+            ->classes(Selector::inNamespace('Makeitlv\Lavstore\Module\.*\Infrastructure'))
+            ->because('Application layer in all modules should only depend on Domain layer');
+    }
+
+    public function testModuleInfrastructureDependsOnDomainAndApplication(): Rule
+    {
+        return PHPat::rule()
+            ->classes(Selector::inNamespace('Makeitlv\Lavstore\Module\.*\Infrastructure'))
+            ->canOnlyDependOn()
+            ->classes(
+                Selector::inNamespace('Makeitlv\Lavstore\Module\.*\Domain'),
+                Selector::inNamespace('Makeitlv\Lavstore\Module\.*\Application')
+            )
+            ->because('Infrastructure layer in all modules should depend on Domain and Application layers');
+    }
+
+    public function testModulesCanOnlyCommunicateThroughAdapters(): Rule
+    {
+        return PHPat::rule()
+            ->classes(Selector::inNamespace('Makeitlv\Lavstore\Modules\.*'))
+            ->excluding(Selector::inNamespace('Makeitlv\Lavstore\Modules\.*\Infrastructure\Adapter\.*'))
+            ->shouldNotDependOn()
+            ->classes(Selector::inNamespace('Makeitlv\Lavstore\Modules\.*'))
+            ->because('Modules should not depend on each other, except through the Adapters');
     }
 }
